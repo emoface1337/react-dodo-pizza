0e25a9934485438a2762c51b44a5887519c4d463
emoface1337 <chvrches@mail.ru>
2020-08-24 22:42:36 +0300

    added json-server, pizzas rendering with prop-types

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Route } from 'react-router-dom'
+import axios from 'axios'
 
 import Header from './components/Header/Header'
 import Home from "./pages/Home"
 import Cart from "./pages/Cart"
 
 const App = () => {
+
+    const [pizzas, setPizzas] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:7777/pizzas')
+        .then(({ data }) => setPizzas(data))
+    }, [])
+
     return (
         <div className="wrapper">
             <Header/>
             <div className="content">
-                <Route exact path='/' component={Home}/>
+                <Route exact path='/' render={() => <Home pizzas={pizzas}/>}/>
                 <Route exact path='/cart' component={Cart}/>
             </div>
         </div>
